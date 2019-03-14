7939e737eef1e5709c9d852c47e67b8bf3d721c3
Renee Deka Ambia <dekadekadeka@gmail.com>
2019-03-14 18:39:12 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,7 +2,7 @@ board = ["O", " ", " ", " ", "X", " ", " ", " ", "X"]
 def turn_count(board)
   count = 0
   board.each do |turn|
-    while turn != " "
+    if turn != " "
     count += 1
     end
   return count
