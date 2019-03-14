a8c55217d58555a2331f11da77a6a8a2e72ed66f
Renee Deka Ambia <dekadekadeka@gmail.com>
2019-03-14 18:23:18 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,16 @@
+def turn_count(board)
+  count = 0
+  board.each do |turn|
+    if turn == "X" || turn == "O"
+    count += 1
+    end
+  end
+end
+
+#def current_player(board)
+#  if turn_count(board) % 2 == 0
+#    "X"
+#  else
+#    "O"
+#  end
+#end
